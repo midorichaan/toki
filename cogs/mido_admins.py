7e86e0533de9a7b225c71a2eb02d69ce02ea4718
midorichan <furandorusukaret.jp@gmail.com>
2022-04-21 14:49:19 +0900

    [Fix] conv

--- cogs/mido_admins.py
+++ cogs/mido_admins.py
@@ -34,7 +34,7 @@ class mido_admins(commands.Cog):
         disk = psutil.disk_usage(path="/")
 
         def convert_gb(v):
-            return v / 1024 * 1024 * 1024
+            return v / 1024**2
 
         e.add_field(name="> __**CPU Status**__", value=f"Core: {cpucore} \nPercent: {cpuper}%")
         e.add_field(
