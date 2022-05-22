f3f214c514f7edc3ed2801b0ad5a73eb1f804dc2
midorichan <furandorusukaret.jp@gmail.com>
2022-05-23 01:56:32 +0900

    [add] ticket cog file

---
+++ cogs/mido_ticket.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+from lib import utils, ticketutils
+
+class mido_ticket(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.ticketutil = ticketutils.TicketUtils(bot)
+
+    #ticket
+    @commands.group(
+        name="ticket",
+        description="チケット関連のコマンドです"
+    )
+    async def _ticket(self, ctx):
+        pass
+
+def setup(bot):
+    bot.add_cog(mido_ticket(bot))
