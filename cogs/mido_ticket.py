fde0500dbce0e466f5fabba6044ec98faca5b1a7
midorichan <furandorusukaret.jp@gmail.com>
2022-05-23 09:38:24 +0900

    [Add] help command

--- cogs/mido_ticket.py
+++ cogs/mido_ticket.py
@@ -13,9 +13,36 @@ class mido_ticket(commands.Cog):
     @commands.group(
         name="ticket",
         description="チケット関連のコマンドです"
+        usage="ticket [args]"
     )
     async def _ticket(self, ctx):
-        pass
+        if ctx.invoked_subcommand is None:
+            return await utils.reply_or_send(ctx, content="> `.ticket help` でヘルプを参照してね！")
+
+    #ticket help
+    @_ticket.command(
+        name="help",
+        description="チケットのヘルプを表示するよ！"
+        usage="ticket help [command]"
+    )
+    async def _help(self, ctx, command: str=None):
+        m = await utils.reply_or_send(ctx, content="> 処理中...")
+        e = discord.Embed(title="Ticket Help", timestamp=ctx.message.created_at)
+
+        if command:
+            ticket = self.bot.get_command("ticket")
+            cmd = ticket.get_command(command)
+            if cmd:
+                e.title = f"Ticket help - {command}"
+                e.add_field(name="使用法", value=cmd.usage or "なし")
+                e.add_field(name="エイリアス", value=", ".join([f"`{row}`" for row in cmd.aliases]) or "なし")
+                e.add_field(name="説明", value=f"```\n{cmd.description}\n```" or "```\n説明なし\n```", inline=False)
+            else:
+                return await m.edit(content=f"> `{command}` というコマンドは存在しないよ！")
+        else:
+            for i in ticket.commands:
+                e.add_field(name=i.name, value=i.description or "説明なし")
+        return await m.edit(content=None, embed=e)
 
 async def setup(bot):
     await bot.add_cog(mido_ticket(bot))
